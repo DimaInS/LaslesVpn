89110c293c6694f42a72bfc848152d78b7c9e854
DimaLnS <dmadamchuk@gmail.com>
2022-03-11 20:33:30 +0200

    added reviews subscribe

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function() {
- 
+  $('.reviews__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: false,
+    dots: true,
+    appendArrows: $('.reviws__arrows'),
+    appendDots: $('.reviws__dots'),
+  
+  });
 });
